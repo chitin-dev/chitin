78dc5f62b7149c1a35b080e4f3201dac00b6dd43
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-07-15 23:12:29 +0800

    feat(desktop): use window bar to desktop (#2)

--- crates/chitin-desktop/src/components/mod.rs
+++ crates/chitin-desktop/src/components/mod.rs
@@ -1,1 +1,2 @@
 pub mod activity_bar;
+pub mod window_bar;
---
+++ crates/chitin-desktop/src/components/window_bar.rs
@@ -0,0 +1,23 @@
+use chitin_ui::{
+  components::window_bar::{WindowBar, WindowBarItem, WindowBarSubtitlePosition},
+  themes::builtins,
+};
+use gpui::{Context, IntoElement};
+
+use crate::app::ChitinApp;
+
+pub fn render_window_bar(_cx: &mut Context<ChitinApp>) -> impl IntoElement {
+  let theme = builtins::dark();
+
+  WindowBar::new("Chitin", "logo-app.svg", WindowBarSubtitlePosition::Right)
+    .theme(theme)
+    .subtitle("open your project")
+    .items([
+      WindowBarItem::new("minimize", "Minimize", "icons/window-bar/lucide-minus.svg")
+        .on_click(|_, window, _| window.minimize_window()),
+      WindowBarItem::new("maximize", "Maximize", "icons/window-bar/lucide-square.svg")
+        .on_click(|_, window, _| window.zoom_window()),
+      WindowBarItem::new("close", "Close", "icons/window-bar/lucide-x.svg")
+        .on_click(|_, _, cx| cx.quit()),
+    ])
+}
